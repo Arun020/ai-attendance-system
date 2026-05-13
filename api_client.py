eb45dd82eb516f54351da891e4979b1df8a60b46
Arun <arun.a.suresh.kumar.ext@Polyvantis.com>
2026-05-14 00:56:20 +0530

    fix: remove all localhost dependencies for cloud deployment

--- api_client.py
+++ api_client.py
@@ -1,7 +1,7 @@
 import requests
 import sys
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "http://https://ai-attendance-system-edyg.onrender.com"
 
 
 # =========================================================
